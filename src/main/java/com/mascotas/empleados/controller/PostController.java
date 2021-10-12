d70c9c399ae481072e540992b97100c3970ea6cf
Ocrezzu <davidjrf95@gmail.com>
2021-10-12 11:50:24 -0500

    trasporte

--- src/main/java/com/mascotas/empleados/controller/PostController.java
+++ src/main/java/com/mascotas/empleados/controller/PostController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.concurrent.ExecutionException;
 
-@CrossOrigin(origins = "*", methods= {RequestMethod.GET,RequestMethod.POST,RequestMethod.DELETE,RequestMethod.PUT})
+@CrossOrigin(origins = "*")
 @RestController
 public class PostController {
 
